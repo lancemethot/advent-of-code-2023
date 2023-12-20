cd486f8ea4a72a55cbdb20a6bfe2155844318a91
LANCE METHOT <LANCE.METHOT@LIBERTYMUTUAL.COM>
2023-12-19 19:14:10 -0500

    day 12 part 1

--- day12/day12.test.ts
+++ day12/day12.test.ts
@@ -1,37 +1,63 @@
-import { getSmallInput } from "../utils";
+import { isEqual } from "lodash";
+import { getFullInput, getSmallInput } from "../utils";
 
 enum Condition {
-    OPERATIONAL = '.',
-    DAMAGED = '#',
-    UNKNOWN = '?',
-};
+  OPERATIONAL = ".",
+  DAMAGED = "#",
+  UNKNOWN = "?",
+}
 
 type Record = {
-    springs: Condition [];
-    sizes: number[];
+  springs: string;
+  sizes: number[];
+};
+
+function parseInput(lines: string[]): Record[] {
+  return lines.map((line) => {
+    return {
+      springs: line.split(" ")[0],
+      sizes: line
+        .split(" ")[1]
+        .split(",")
+        .map((size) => parseInt(size)),
+    };
+  });
 }
 
-function parseInput(lines: string[]): Record [] {
-    return lines.map((line) => {
-        return {
-            springs: line.split(' ')[0].split('') as Condition[],
-            sizes: line.split(' ')[1].split(',').map((size)=> parseInt(size)),
-        }
-    });
+function generateCombinations(input: string): string[] {
+  const index = input.indexOf(Condition.UNKNOWN);
+  if (index === -1) {
+    return [input];
+  }
+  const prefix = input.substring(0, index);
+  const suffix = input.substring(index + 1);
+  const combinationsAsOperational = generateCombinations(
+    prefix + Condition.OPERATIONAL + suffix
+  );
+  const combinationsAsDamaged = generateCombinations(
+    prefix + Condition.DAMAGED + suffix
+  );
+  return [...combinationsAsOperational, ...combinationsAsDamaged];
 }
 
-function determineArrangements(record: Record): Record[][] {
-    console.log(`checking: ${record.springs.join('')} for groups: ${record.sizes.join(',')}`);
-    return [];
+function determineArrangements(record: Record): number {
+  return generateCombinations(record.springs).filter((combination) => {
+    const combinationSizes = combination
+      .split(Condition.OPERATIONAL)
+      .filter((group) => group.length > 0)
+      .map((group) => group.split("").length);
+    return isEqual(combinationSizes, record.sizes);
+  }).length;
 }
 
 function partOne(lines: string[]): number {
-    return parseInput(lines).reduce((acc, record) => {
-        return acc += determineArrangements(record).length;
-    }, 0);
+  return parseInput(lines).reduce((acc, record) => {
+    return (acc += determineArrangements(record));
+  }, 0);
 }
 
-const day = 'day12';
+const day = "day12";
 test(day, () => {
-    expect(partOne(getSmallInput(day))).toBe(21);
+  expect(partOne(getSmallInput(day))).toBe(21);
+  expect(partOne(getFullInput(day))).toBe(7251);
 });
