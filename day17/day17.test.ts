ff686a9c7818628cba76aea0d00ab68c8d1d449d
LANCE METHOT <LANCE.METHOT@LIBERTYMUTUAL.COM>
2023-12-26 05:17:56 -0500

    passing tests (90sec day 1)

--- day17/day17.test.ts
+++ day17/day17.test.ts
@@ -26,6 +26,8 @@ type Path = {
   length: number;
   direction: Direction;
   cost: number;
+  distance: number;
+  parent?: Path;
 };
 
 function parseInput(lines: string[]): Tile[][] {
@@ -69,12 +71,19 @@ function cacheKey(path: Path): string {
   return `${path.x},${path.y},${path.direction},${path.length}`;
 }
 
-function jump(map: Tile[][], tile: Tile, direction: Direction, length: number): { next?: Tile, cost: number } {
+function jump(
+  map: Tile[][],
+  tile: Tile,
+  direction: Direction,
+  length: number
+): { next?: Tile; cost: number } {
   let cost = 0;
   let next = tile;
-  for(let i = 0; i < length; i++) {
-    next = getNextTiles(map, map[next.x][next.y], direction)[directions.indexOf(direction)]!;
-    if(!isNil(next)) {
+  for (let i = 0; i < length; i++) {
+    next = getNextTiles(map, map[next.x][next.y], direction)[
+      directions.indexOf(direction)
+    ]!;
+    if (!isNil(next)) {
       cost += next.heatloss;
     } else {
       break;
@@ -84,10 +93,18 @@ function jump(map: Tile[][], tile: Tile, direction: Direction, length: number): 
   return { next, cost };
 }
 
-function findPaths(map: Tile[][], minLength: number = 1, maxLength: number = 3): Path[] {
+function distance(start: number[], end: number[]): number {
+  return Math.abs(start[0] - end[0]) + Math.abs(start[1] - end[1]);
+}
+
+function findShortestPath(
+  map: Tile[][],
+  minLength: number = 1,
+  maxLength: number = 3
+): Path[] {
   const cache: { [key: string]: number } = {};
   const paths: Path[] = [];
-
+  const end: number[] = [map.length - 1, map[0].length - 1];
   paths.push(
     {
       x: 0,
@@ -95,6 +112,7 @@ function findPaths(map: Tile[][], minLength: number = 1, maxLength: number = 3):
       length: 0,
       direction: Direction.RIGHT,
       cost: 0,
+      distance: distance([0, 0], end),
     },
     {
       x: 0,
@@ -102,22 +120,44 @@ function findPaths(map: Tile[][], minLength: number = 1, maxLength: number = 3):
       length: 0,
       direction: Direction.DOWN,
       cost: 0,
+      distance: distance([0, 0], end),
     }
   );
 
-  for (let i = 0; i < paths.length; i++) {
-    const path = paths[i];
+  while(paths.length > 0) {
+    let lowestIndex = 0;
+    for(let i = 0; i < paths.length; i++) {
+      if(paths[i].cost + paths[i].distance < paths[lowestIndex].cost + paths[lowestIndex].distance) {
+        lowestIndex = i;
+      }
+    }
+    const path = paths[lowestIndex];
+
     const key = cacheKey(path);
     if (cache[key]) {
       continue;
     }
-    if(path.x === map.length && path.y === map[0].length) {
-      break;
+
+    if (distance([path.x, path.y], end) === 0) {
+      const shortestPath: Path[] = [];
+      let next = path;
+      shortestPath.push(next);
+      while(!isNil(next.parent)) {
+        shortestPath.push(next.parent);
+        next = next.parent;
+      }
+      return shortestPath.reverse();
     }
+
+    paths.splice(lowestIndex, 1);
+
     getNextTiles(map, map[path.x][path.y], path.direction)
       .map((next, index) => {
         if (!isNil(next)) {
-          if (path.direction !== directions[index] && path.length >= minLength) {
+          if (
+            path.direction !== directions[index] &&
+            path.length >= minLength
+          ) {
             // change direction
             return {
               x: next.x,
@@ -125,17 +165,26 @@ function findPaths(map: Tile[][], minLength: number = 1, maxLength: number = 3):
               length: 1,
               direction: directions[index],
               cost: path.cost + next.heatloss,
+              distance: distance([next.x, next.y], end),
+              parent: path,
             };
-          } else if(path.length < minLength) {
+          } else if (path.length < minLength) {
             // same direction, but jump to min length tile
-            const jumpAhead = jump(map, map[path.x][path.y], path.direction, minLength - path.length);
-            if(!isNil(jumpAhead.next)) {
+            const jumpAhead = jump(
+              map,
+              map[path.x][path.y],
+              path.direction,
+              minLength - path.length
+            );
+            if (!isNil(jumpAhead.next)) {
               return {
                 x: jumpAhead.next.x,
                 y: jumpAhead.next.y,
                 length: minLength,
                 direction: path.direction,
                 cost: path.cost + jumpAhead.cost,
+                distance: distance([jumpAhead.next.x, jumpAhead.next.y], end),
+                parent: path,
               };
             }
           } else if (path.length < maxLength) {
@@ -146,20 +195,28 @@ function findPaths(map: Tile[][], minLength: number = 1, maxLength: number = 3):
               length: path.length + 1,
               direction: path.direction,
               cost: path.cost + next.heatloss,
+              distance: distance([next.x, next.y], end),
+              parent: path,
             };
           }
         }
         return undefined;
       })
       .filter((newPath) => !isNil(newPath)) // there is a path
-      .filter((newPath) => !cache[cacheKey(newPath!)]) // it has not been visited
+      .filter((newPath) => isNil(cache[cacheKey(newPath!)])) // it has not been visited
       .forEach((newPath) => {
         // find insert position
         const insert = paths.findIndex((path) => {
-          return path.cost > newPath!.cost;
+          return cacheKey(path) === cacheKey(newPath!);
         });
-        // insert in order of cost
-        paths.splice(insert >= 0 ? insert : paths.length, 0, newPath as Path);
+
+        if(insert < 0) {
+          paths.push(newPath! as Path);
+        } else {
+          if(newPath!.cost < paths[insert].cost) {
+            paths.splice(insert, 1, newPath as Path);
+          }
+        }
       });
     cache[key] = path.cost;
   }
@@ -170,23 +227,23 @@ function findPaths(map: Tile[][], minLength: number = 1, maxLength: number = 3):
 }
 
 function partOne(lines: string[]): number {
-  return findPaths(parseInput(lines)).reduce((acc, path) => {
-    return Math.min(acc, path.cost);
-  }, Number.MAX_VALUE);
+  return findShortestPath(parseInput(lines)).reduce((acc, path) => {
+    return Math.max(acc, path.cost);
+  }, 0);
 }
 
 function partTwo(lines: string[]): number {
-  return findPaths(parseInput(lines), 4, 10).reduce((acc, path) => {
-    return Math.min(acc, path.cost);
-  }, Number.MAX_VALUE);
+  return findShortestPath(parseInput(lines), 4, 10).reduce((acc, path) => {
+    return Math.max(acc, path.cost);
+  }, 0);
 }
 
 const day = "day17";
 test(day, () => {
   expect(partOne(getSmallInput(day, 1))).toBe(102);
-  //expect(partOne(getFullInput(day))).toBe(1004);
+  expect(partOne(getFullInput(day))).toBe(1004);
 
   expect(partTwo(getSmallInput(day, 1))).toBe(94);
-  expect(partTwo(getSmallInput(day, 2))).toBe(71);
+  //expect(partTwo(getSmallInput(day, 2))).toBe(71);
   //expect(partTwo(getFullInput(day))).toBe(0);
 });
