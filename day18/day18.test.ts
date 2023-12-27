9caa5657729eaa11ccbe46e115c5dfbcf2f3a34b
LANCE METHOT <LANCE.METHOT@LIBERTYMUTUAL.COM>
2023-12-27 08:17:31 -0500

    day 18 part 2 incomplete

--- day18/day18.test.ts
+++ day18/day18.test.ts
@@ -1,3 +1,4 @@
+import { isNil } from "lodash";
 import { getFullInput, getSmallInput } from "../utils";
 
 enum Direction {
@@ -6,6 +7,8 @@ enum Direction {
     UP = 'U',
     DOWN = 'D',
 }
+
+const directions = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP];
 
 enum Symbol {
   DUG = '#',
@@ -19,7 +22,24 @@ enum CornerType {
   LEFT_DOWN = 'F', 
 }
 
+type Corner = {
+  x: number;
+  y: number;
+  direction: Direction;
+  cornerType?: CornerType;
+  up?: Corner;
+  down?: Corner;
+  left?: Corner;
+  right?: Corner;
+}
+
 type DigSite = {
+  height: number;
+  width: number;
+  corners: Corner[];
+}
+
+type Tile = {
     x: number;
     y: number;
     color?: string;
@@ -46,11 +66,21 @@ function parseInput(lines: string[]): DigPlan[] {
     });
 }
 
-function cornerType(site: DigSite[][], x: number, y: number): CornerType | undefined {
-  const north: DigSite | undefined = x > 0 ? site[x-1][y] : undefined;
-  const south: DigSite | undefined = x < site.length - 1 ? site[x+1][y] : undefined;
-  const west: DigSite | undefined = y > 0 ? site[x][y-1] : undefined;
-  const east: DigSite | undefined = y < site[x].length - 1 ? site[x][y+1] : undefined;
+function adjustDigPlan(plan: DigPlan[]): DigPlan[] {
+  return plan.map(next => {
+    return {
+      ... next,
+      direction: directions[parseInt(next.color.substring(next.color.length - 1))],
+      length: Number(`0x${next.color.substring(1, next.color.length - 1)}`)
+    }
+  });
+}
+
+function cornerType(site: Tile[][], x: number, y: number): CornerType | undefined {
+  const north: Tile | undefined = x > 0 ? site[x-1][y] : undefined;
+  const south: Tile | undefined = x < site.length - 1 ? site[x+1][y] : undefined;
+  const west: Tile | undefined = y > 0 ? site[x][y-1] : undefined;
+  const east: Tile | undefined = y < site[x].length - 1 ? site[x][y+1] : undefined;
 
   if(north?.symbol === Symbol.DUG && east?.symbol === Symbol.DUG) {
     // L shaped corners
@@ -69,12 +99,12 @@ function cornerType(site: DigSite[][], x: number, y: number): CornerType | undef
 
 // Check if a corner is a 'bad' corner for digTrench logic
 //    e.g. 'L' or '7' shaped corners
-function isTrickyCorner(site: DigSite[][], x: number, y: number): boolean {
+function isTrickyCorner(site: Tile[][], x: number, y: number): boolean {
     return site[x][y].corner !== undefined && (site[x][y].corner === CornerType.DOWN_RIGHT || site[x][y].corner === CornerType.RIGHT_DOWN);
 }
 
 // Diagonal dig pattern
-function digTrench(site: DigSite[][]): DigSite[][] {
+function digTrench(site: Tile[][]): Tile[][] {
   let edgesFound = 0;
   // Start at bottom left corner of grid and move up
   for (let i = site.length - 1; i >= 0; i--) {
@@ -126,7 +156,7 @@ function digTrench(site: DigSite[][]): DigSite[][] {
   return site;
 }
 
-function digBorder(plan: DigPlan[]): DigSite[][] {
+function digBorder(plan: DigPlan[]): Tile[][] {
     let minX: number = 0, maxX: number = 0, x: number = 0;
     let minY: number = 0, maxY: number = 0, y: number = 0;
     for(let i = 0; i < plan.length; i++) {
@@ -143,13 +173,13 @@ function digBorder(plan: DigPlan[]): DigSite[][] {
     const width = maxY - minY + 1;
 
     // Prepare a dig site and clear it
-    let site: DigSite[][] = Array(height).fill([]);
+    let site: Tile[][] = Array(height).fill([]);
     for(let i = 0; i < height; i++) {
         site[i] = Array(width).fill([]);
     }
     site = site.map((row, x) => row.map((cell, y) => {
         // init to empty
-        return { x, y, symbol: Symbol.UNDUG } as DigSite;
+        return { x, y, symbol: Symbol.UNDUG } as Tile;
     }));
 
     // Dig the border
@@ -167,7 +197,7 @@ function digBorder(plan: DigPlan[]): DigSite[][] {
 
     // Identify corner types
     site = site.map((row, x) => row.map((cell, y) => {
-        return cell.color ? { ...cell, corner: cornerType(site, x, y) } as DigSite : cell;
+        return cell.color ? { ...cell, corner: cornerType(site, x, y) } as Tile : cell;
     }));
 
     //console.log('border:');
@@ -175,8 +205,82 @@ function digBorder(plan: DigPlan[]): DigSite[][] {
     return site;
 }
 
-function dig(plan: DigPlan[]): DigSite[][] {
+
+///// PART 2 WORK /////
+///// PART 2 WORK /////
+///// PART 2 WORK /////
+
+function prepareSite(plan: DigPlan[]): DigSite {
+  let minX: number = 0, maxX: number = 0, x: number = 0;
+  let minY: number = 0, maxY: number = 0, y: number = 0;
+
+  let corners: Corner[] = plan.map(next => {
+    // parse plan
+    const corner: Corner = { x, y, direction: next.direction };
+    x += next.direction === Direction.DOWN ? next.length : next.direction === Direction.UP ? -next.length : 0;
+    y += next.direction === Direction.RIGHT ? next.length : next.direction === Direction.LEFT ? -next.length : 0;
+    minX = Math.min(minX, x);
+    maxX = Math.max(maxX, x);
+    minY = Math.min(minY, y);
+    maxY = Math.max(maxY, y);
+    return corner;
+  }).map((corner, i, corners) => {
+    // establish relationships
+    let next = i < corners.length - 1 ? i + 1 : 0;
+    switch(corner.direction) {
+      case Direction.DOWN:
+        corner.down = corners[next];
+        corners[next].up = corner;
+        break;
+      case Direction.UP:
+        corner.up = corners[next];
+        corners[next].down = corner;
+        break;
+      case Direction.LEFT:
+        corner.left = corners[next];
+        corners[next].right = corner;
+        break;
+      case Direction.RIGHT:
+        corner.right = corners[next];
+        corners[next].left = corner;
+        break;
+    }
+    return corner;
+  }).map(corner => {
+    // identify corner types
+    if(corner.up && corner.right) {
+      corner.cornerType = CornerType.DOWN_RIGHT; // L
+    } else if(corner.left && corner.down) {
+      corner.cornerType = CornerType.RIGHT_DOWN; // 7
+    } else if(corner.up && corner.left) {
+      corner.cornerType = CornerType.DOWN_LEFT;  // J
+    } else {
+      corner.cornerType = CornerType.LEFT_DOWN;  // F
+    }
+    return corner;
+  });
+
+  return {
+    height: maxX - minX + 1,
+    width: maxY - minY + 1,
+    corners: corners,
+  };
+}
+
+
+///// PART 2 WORK /////
+///// PART 2 WORK /////
+///// PART 2 WORK /////
+
+
+function dig(plan: DigPlan[]): Tile[][] {
   return digTrench(digBorder(plan));
+}
+
+function dig2(plan: DigPlan[]): number {
+  const site = prepareSite(plan);
+  console.log(site.corners.map(corner => `${corner.x}, ${corner.y} type: ${corner.cornerType} u: ${isNil(corner.up) ? 'no': 'yes'} d: ${isNil(corner.down) ? 'no': 'yes'} l: ${isNil(corner.left) ? 'no': 'yes'} r: ${isNil(corner.right) ? 'no': 'yes'}`).join('\n'));
+  return 0;
 }
 
 function partOne(lines: string[]): number {
@@ -185,8 +289,14 @@ function partOne(lines: string[]): number {
     }, 0);
 }
 
+function partTwo(lines: string[]): number {
+  return dig2(adjustDigPlan(parseInput(lines)));
+}
+
 const day = 'day18';
 test(day, () => {
     expect(partOne(getSmallInput(day))).toBe(62);
     expect(partOne(getFullInput(day))).toBe(50603);
+
+    expect(partTwo(getSmallInput(day))).toBe(952408144115);
 })
