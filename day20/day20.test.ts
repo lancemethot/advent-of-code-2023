ba755e16082a02aaf15e84f1fc09ff2f915a389d
LANCE METHOT <LANCE.METHOT@LIBERTYMUTUAL.COM>
2024-01-01 09:43:05 -0500

    day 20 part 2 complete

--- day20/day20.test.ts
+++ day20/day20.test.ts
@@ -93,9 +93,70 @@ function pushButton(modules: Map<string, Module>): number {
     return lowPulses * highPulses;
 }
 
+function gcd(a: number, b: number): number {
+    return a ? gcd(b % a, a) : b;
+}
+
+function lcm(a: number, b: number): number {
+    return a * b / gcd(a, b);
+}
+
+function cycle(modules: Map<string, Module>): number {
+    const pulses: Pulse[] = [];
+    let pulse: Pulse | undefined;
+    let cycles = 0;
+    let index = 1;
+    let detector: {[key: string]: number} = {
+        hf: 0,  // These 4 modules will active vf
+        mk: 0,  // when all are sending 'low'
+        pk: 0,  // keep track of how many cycles
+        pm: 0,  // it takes to activate each
+    }
+    while(++cycles > 0) {
+        pulses.push({ pulseType: PulseType.LOW, source: 'button', destination: 'broadcaster'});
+        while((pulse = pulses.shift()) !== undefined) {
+            index = Math.max(0, index - 1);
+            const module = modules.get(pulse.destination);
+            if (!isNil(module)) {
+                if(module.moduleType === ModuleType.FLIP_FLOP) {
+                    if(pulse.pulseType === PulseType.LOW) {
+                        module.on = !module.on;
+                        let send: PulseType = module.on ? PulseType.HIGH : PulseType.LOW;
+                        pulses.splice(index, 0, ... module.destinations.map((dest) => ({ pulseType: send, source: module.name, destination: dest })));
+                        index += module.destinations.length;
+                    }
+                } else if(module.moduleType === ModuleType.CONJUNCTION) {
+                    module.memory[pulse.source] = pulse.pulseType;
+                    let send: PulseType = Object.keys(module.memory).every((key) => module.memory[key] === PulseType.HIGH) ? PulseType.LOW : PulseType.HIGH;
+                    // Break when all detectors have been triggered
+                    if(detector[module.name] === 0 && send === PulseType.HIGH) {
+                        detector[module.name] = cycles;
+                        if(Object.keys(detector).every((key) => detector[key] > 0)) {
+                            return [detector.hf, detector.mk, detector.pk, detector.pm].reduce(lcm);
+                        }
+                    }
+                    pulses.splice(index, 0, ... module.destinations.map((dest) => ({ pulseType: send, source: module.name, destination: dest })));
+                    index += module.destinations.length;
+                } else {
+                    // broadcaster
+                    pulses.splice(index, 0, ...module.destinations.map((dest) => ({ pulseType: pulse!.pulseType, source: module.name, destination: dest })));
+                    index += module.destinations.length;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
 function partOne(lines: string[]): number {
     const modules = initialize(parseInput(lines));
     return pushButton(modules);
+}
+
+function partTwo(lines: string[]): number {
+    const modules = initialize(parseInput(lines));
+    return cycle(modules);
 }
 
 const day = 'day20';
@@ -103,4 +164,6 @@ test(day, () => {
     expect(partOne(getSmallInput(day, 1))).toBe(32000000);
     expect(partOne(getSmallInput(day, 2))).toBe(11687500);
     expect(partOne(getFullInput(day))).toBe(680278040);
+
+    expect(partTwo(getFullInput(day))).toBe(243548140870057);
 });
