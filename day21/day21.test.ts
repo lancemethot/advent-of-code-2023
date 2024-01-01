8a4d650bcff16d24cd2d0e0ec2d84dc1faefb45f
LANCE METHOT <LANCE.METHOT@LIBERTYMUTUAL.COM>
2024-01-01 12:38:57 -0500

    fixed failing day 21 part 1 test setup part 2

--- day21/day21.test.ts
+++ day21/day21.test.ts
@@ -10,18 +10,20 @@ type Tile = {
   x: number;
   y: number;
   symbol: TileType;
+  xquad: number;
+  yquad: number;
 };
 
 function parseInput(lines: string[]): Tile[][] {
   return lines.map((line, x) => {
     return line.split("").map((symbol, y) => {
-      return { x, y, symbol: symbol as TileType };
+      return { x, y, symbol: symbol as TileType, xquad: 0, yquad: 0 };
     });
   });
 }
 
 function startTile(tiles: Tile[][]): Tile {
-  let start: Tile = { x: -1, y: -1, symbol: TileType.START };
+  let start: Tile = { x: -1, y: -1, symbol: TileType.START , xquad: 0, yquad: 0};
   return tiles.reduce((acc, line) => {
     return line.reduce((acc, tile) => {
       if (tile.symbol === TileType.START) {
@@ -62,8 +64,27 @@ function partOne(lines: string[], steps: number): number {
   return next.length;
 }
 
+function partTwo(lines: string[], steps: number): number {
+  let tiles = parseInput(lines);
+  let start: Tile = startTile(tiles);
+  let next: Tile[] = [start];
+  for (let i = 0; i < steps; i++) {
+    next = step(tiles, next);
+  }
+  return next.length;
+}
+
 const day = "day21";
 test(day, () => {
   expect(partOne(getSmallInput(day), 6)).toBe(16);
-  expect(partOne(getFullInput(day), 64)).toBe(0);
+  expect(partOne(getFullInput(day), 64)).toBe(3660);
+
+  expect(partTwo(getSmallInput(day), 6)).toBe(16);
+  //expect(partTwo(getSmallInput(day), 10)).toBe(50);
+  //expect(partTwo(getSmallInput(day), 50)).toBe(1594);
+  //expect(partTwo(getSmallInput(day), 100)).toBe(6536);
+  //expect(partTwo(getSmallInput(day), 500)).toBe(167004);
+  //expect(partTwo(getSmallInput(day), 1000)).toBe(668697);
+  //expect(partTwo(getSmallInput(day), 5000)).toBe(16733044);
+  //expect(partTwo(getFullInput(day), 26501365)).toBe(0);
 });
