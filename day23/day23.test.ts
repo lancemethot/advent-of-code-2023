6f8aea9411895a96b678d1aa77ab11484494f9ce
LANCE METHOT <LANCE.METHOT@LIBERTYMUTUAL.COM>
2024-01-12 16:13:49 -0500

    refactored day 23 parts to use common traversal

--- day23/day23.test.ts
+++ day23/day23.test.ts
@@ -13,7 +13,6 @@ type Tile = {
     type: TileType,
     x: number,
     y: number,
-    distance: number,
 }
 
 type Segment = {
@@ -38,6 +37,9 @@ function parseInput(lines: string[]): Tile[][] {
     });
 }
 
+// TODO: return all four directions regardless of undefined
+// TODO: prefer perimeter options during traversal
+
 function moves(tiles: Tile[][], tile: Tile, slippery: boolean = true): Tile[] {
     let north: Tile | undefined = tiles[tile.x-1] ? tiles[tile.x-1][tile.y] : undefined;
     let south: Tile | undefined = tiles[tile.x+1] ? tiles[tile.x+1][tile.y] : undefined;
@@ -58,34 +60,11 @@ function moves(tiles: Tile[][], tile: Tile, slippery: boolean = true): Tile[] {
     return [north, south, east, west].filter(t => t !== undefined).filter(t => t?.type !== TileType.FOREST) as Tile[];
 }
 
-function traverse(tiles: Tile[][], path: Tile[], start: Tile, end: Tile, slippery: boolean = true): Tile[][] {
-    let visited: Tile[] = path.map(t => { return { ...t } }).concat([{ ...start }]);
-    visited[visited.length-1].distance = visited[visited.length-2]?.distance + 1 || 0;
-    let index = visited.length - 1;
-    while(index < visited.length) {
-        let current = visited[index];
-        if(current.x === end.x && current.y === end.y) {
-            return [visited];
-        }
-        let next = moves(tiles, current, slippery).filter(tile => !visited.some(v => v.x === tile.x && v.y === tile.y));
-        if(next.length === 1) {
-            visited.push({
-                ...next[0],
-                distance: current.distance + 1,
-            });
-        } else {
-            return next.reduce((acc, tile) => {
-                return acc.concat(traverse(tiles, visited, tile, end, slippery));
-            }, [] as Tile[][]);
-        }
-        index++;
-    }
-    return [visited];
-}
-
 // Contract path into segments of straight lines that each have a length
 // and each branch into other segments.
-function contract(tiles: Tile[][], start: Tile, end: Tile, slippery: boolean = false): Segment[] {
+function contract(tiles: Tile[][], slippery: boolean = false): Segment[] {
+    let start = tiles[0].filter(tile => tile.type === TileType.PATH)[0];
+    let end = tiles[tiles.length-1].filter(tile => tile.type === TileType.PATH)[0];
     let segments: Segment[] = [];
     segments.push({ from: start, start, end: start, length: -1, branches: [], exit: false });
     for(let sIndex = 0; sIndex < segments.length; sIndex++) {
@@ -131,7 +110,7 @@ function contract(tiles: Tile[][], start: Tile, end: Tile, slippery: boolean = f
     return segments;
 }
 
-function traverseSegments(segments: Segment[], start: Tile): Segment[][] {
+function traverse(segments: Segment[], start: Tile): Segment[][] {
     let paths: Segment[][] = [];
 
     let first = segments.filter(s => s.start.x === start.x && s.start.y === start.y)[0];
@@ -172,7 +151,7 @@ function printMap(tiles: Tile[][], segments: Segment[]) {
         let y = segments[s].start.y;
         for(let i = 0; i < segments[s].length; i++) {
             map[x][y] = 'O';
-            let next = moves(tiles, { x, y, type: TileType.PATH, distance: 0 }, false).filter(tile => map[tile.x][tile.y] !== 'O');
+            let next = moves(tiles, { x, y, type: TileType.PATH }, false).filter(tile => map[tile.x][tile.y] !== 'O');
             if(next.length > 0) {
                 x = next[0].x;
                 y = next[0].y;
@@ -184,22 +163,17 @@ function printMap(tiles: Tile[][], segments: Segment[]) {
 
 function partOne(input: string[]): number {
     let tiles: Tile[][] = parseInput(input);
-    let start = tiles[0].filter(tile => tile.type === TileType.PATH)[0];
-    start.distance = 0;
-    let end = tiles[tiles.length-1].filter(tile => tile.type === TileType.PATH)[0];
-    return traverse(tiles, [], start, end, true).reduce((acc, path) => {
-        return Math.max(acc, path.pop()!.distance);
+    let segments = contract(tiles, true);
+    return traverse(segments, segments[0].start).filter(p => p[p.length - 1].exit).reduce((acc, path) => {
+        let pathLength = path.reduce((acc, segment) => acc + segment.length, 0);
+        return Math.max(acc, pathLength);
     }, 0);
 }
 
 function partTwo(input: string[]): number {
     let tiles: Tile[][] = parseInput(input);
-    let start = tiles[0].filter(tile => tile.type === TileType.PATH)[0];
-    start.distance = 0;
-    let end = tiles[tiles.length-1].filter(tile => tile.type === TileType.PATH)[0];
-    let contracted = contract(tiles, start, end, false);
-    //console.log(contracted.map(segment => `${segment.from.x},${segment.from.y} -> ${segment.start.x},${segment.start.y} ${segment.length} units ${segment.exit ? 'EXIT' : `${segment.branches.length} branches: ${segment.branches.map(b => `${b.x},${b.y}`).join(', ')}`}`).join('\n'));
-    return traverseSegments(contracted, contracted[0].start).filter(p => p[p.length-1].exit).reduce((acc, path, index) => {
+    let segments = contract(tiles, false);
+    return traverse(segments, segments[0].start).filter(p => p[p.length-1].exit).reduce((acc, path, index) => {
         let pathLength = path.reduce((acc, segment) => acc + segment.length, 0);
         return Math.max(acc, pathLength);
     }, 0);
