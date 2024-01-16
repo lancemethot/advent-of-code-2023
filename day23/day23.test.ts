e6110a797deddd53ab1041360f095af35cb82ff9
LANCE METHOT <LANCE.METHOT@LIBERTYMUTUAL.COM>
2024-01-16 16:11:55 -0500

    refactored to graph of junctions

--- day23/day23.test.ts
+++ day23/day23.test.ts
@@ -18,27 +18,31 @@ enum Direction {
 const directions: Direction[] = [Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST];
 
 type Tile = {
-    type: TileType,
-    x: number,
-    y: number,
+    type: TileType;
+    x: number;
+    y: number;
 }
 
 type Junction = {
-    tile: Tile,
-    branches: (Segment | undefined)[],
-    exit: boolean,
+    tile: Tile;
+    branches: Branch[];
+    exit: boolean;
 }
 
-type Segment = {
-    start: Tile,
-    end: Tile,
-    length: number,
-    junctions: Junction[],
+type Branch = {
+    direction: Direction;
+    length: number;
+    start?: Tile;
+    end?: Tile;
+    junction?: Junction;
 }
 
 type Path = {
-    junctions: Junction[],
-    segments: Segment[],
+    steps: {
+        junction: Junction;
+        direction: Direction;
+        length: number;
+    }[];
 }
 
 function parseInput(lines: string[]): Tile[][] {
@@ -48,7 +52,6 @@ function parseInput(lines: string[]): Tile[][] {
                 x,
                 y,
                 type: char as TileType,
-                distance: Number.MAX_VALUE,
             } as Tile
         });
     });
@@ -79,134 +82,124 @@ function moves(tiles: Tile[][], tile: Tile, slippery: boolean = true): (Tile | u
     return [north, south, east, west].map(t => t?.type === TileType.FOREST ? undefined : t) as Tile[];
 }
 
-// Contract the map into Junctions and Segments.
+// Contract the map into Junctions and Branches.
 // Junctions are points where the path splits into multiple directions.
-// Segments are paths between junctions.
+// Branches are paths between junctions.
 function contract(tiles: Tile[][], slippery: boolean = false): Junction[] {
     let start = tiles[0].filter(tile => tile.type === TileType.PATH)[0];
     let end = tiles[tiles.length-1].filter(tile => tile.type === TileType.PATH)[0];
-    let junctions: Junction[] = [];
-    let segments: Segment[] = [];
 
+    let junctions: Junction[] = [];
     junctions.push({ tile: start, branches: [], exit: false });
+
     for(let jIndex = 0; jIndex < junctions.length; jIndex++) {
         let junction = junctions[jIndex];
         let next = moves(tiles, junction.tile, slippery);
         junctions[jIndex].branches = next.map((tile, index) => {
-            if(tile === undefined) return undefined;
-            // Check if we've discovered this segment
-            let discovered = segments.findIndex(s => (s.start.x === tile!.x && s.start.y === tile!.y) || (s.end.x === tile!.x && s.end.y === tile!.y));
-            if(discovered >= 0) {
-                return segments[discovered];
-            } else {
-                let segment: Segment = {
-                    start: tile,
-                    end: tile,
-                    length: 0,
-                    junctions: [junction],
-                };
+            let branch: Branch = {
+                direction: directions[index],
+                start: tile,
+                end: tile,
+                length: 0,
+            };
 
-                // Start walking the segment
-                let visited: Tile[] = [junction.tile, segment.start];
+            if(tile !== undefined) {
+                // Start walking the branch
+                let visited: Tile[] = [junction.tile, branch.start as Tile];
                 for(let vIndex = 1; vIndex < visited.length; vIndex++) {
                     let current: Tile = visited[vIndex];
                     let next = moves(tiles, current, slippery).map(tile => tile && visited.some(v => v.x === tile!.x && v.y === tile!.y) ? undefined : tile);
                     let available = next.filter(tile => tile !== undefined);
 
-                    if(available.length === 0) {
-                        // Exit or dead-end
-                        if(current.x === end.x && current.y === end.y) {
-                            let junction = { tile: current, branches: [], exit: true, deadend: false };
-                            junctions.push(junction);
-                            segment.junctions.push(junctions[junctions.length - 1]);
-                        }
-                        break;
-                    } else if(available.length === 1) {
+                    if(available.length === 1) {
                         // Keep moving forward
-                        segment.end = current;
-                        segment.length++;
+                        branch.end = current;
+                        branch.length++;
                         visited.push(available[0]!);
-                    } else {
-                        // Hit a junction tile
-                        // Check if we've found it already
-                        let existing = junctions.findIndex(j => j.tile.x === current.x && j.tile.y === current.y) ;
-
-                        if(existing < 0) {
-                            // If junction is new, add it to the list
-                            junctions.push({ tile: current, branches: [], exit: false });
-                            segment.junctions.push(junctions[junctions.length - 1]);
-                        } else {
-                            segment.junctions.push(junctions[existing]!);
-                        }
+                        continue;
                     }
-                }
 
-                segments.push(segment);
-                return segment;
+                    // Hit a junction tile
+                    let junction = {
+                        tile: current,
+                        branches: [],
+                        exit: current.x === end.x && current.y === end.y,
+                    }
+
+                    if(available.length === 0 && !junction.exit) {
+                        // Dead-end
+                        break;
+                    }
+
+                    branch.junction = junction;
+
+                    // Check if we've found it already
+                    let existing = junctions.findIndex(j => j.tile.x === current.x && j.tile.y === current.y) ;
+                    if(existing < 0) {
+                        // If junction is new, add it to the list
+                        junctions.push(junction);
+                    }
+                }            
             }
+
+            return branch;
         });
     }
 
     return junctions;
-
 }
 
 function traverse(junctions: Junction[], start: Tile): Path[] {
     let paths: Path[] = [];
     paths.push({
-        junctions: [junctions.filter(j => j.tile.x === start.x && j.tile.y === start.y)[0]],
-        segments: [],
+        steps: [{
+            junction: junctions.filter(j => j.tile.x === start.x && j.tile.y === start.y)[0],
+            direction: Direction.SOUTH,
+            length: 0,
+        }],
     });
 
     for(let pIndex = 0; pIndex < paths.length; pIndex++) {
         let path = paths[pIndex];
-        for(let jIndex = path.junctions.length - 1; jIndex < path.junctions.length; jIndex++) {
-            let junction = path.junctions[jIndex];
+        for(let jIndex = path.steps.length - 1; jIndex < path.steps.length; jIndex++) {
+            let junction = path.steps[jIndex].junction;
             if(junction.exit) continue; // done with path
 
+            // determine direction by checking the previous junction
+            // check with branch was chosen to reach current junction
+            let direction = jIndex === 0 ? Direction.SOUTH : directions[path.steps[jIndex-1]?.junction.branches.findIndex(branch => branch && branch?.junction?.tile.x === junction.tile.x && branch.junction.tile.y === junction.tile.y) || 0];
+
             // Map branches to segments in this order: [north, south, east, west]
-            let next = junction.branches.map(branch => {
-                if(branch === undefined) return undefined;
-                // Skip segments already visited in this path
-                let visitedSegment = path.segments.some(segment => segment.start.x === branch!.start.x && segment.start.y === branch!.start.y && segment.end.x === branch!.end.x && segment.end.y === branch!.end.y);
-                // Skip segments with junctions that have already been visited
-                let visitedJunction = branch.junctions.every(j => path.junctions.some(j2 => j2.tile.x === j.tile.x && j2.tile.y === j.tile.y));
-                return visitedSegment || visitedJunction ? undefined : branch;
+            let branches = junction.branches.filter(b => b !== undefined && b.junction !== undefined).map(branch => {
+                // Skip branches if junction has already been visited
+                let visited = path.steps.some(s => s.junction.tile.x === branch!.junction!.tile.x && s.junction.tile.y === branch!.junction!.tile.y);
+                return visited ? undefined : branch;
+            }).filter(b => b !== undefined).sort((a, b) => {
+                return a?.direction === Direction.SOUTH || a?.direction == Direction.EAST ? -1 : 1;
+            }).sort((a, b) => {
+                return direction === a?.direction ? -1 : direction === b?.direction ? 1 : 0;
             });
 
-            let available = next.filter(segment => segment !== undefined).length;
-
-            if(available === 0) {
+            if(branches.length === 0) {
                 // no moves, remove from paths
                 paths.splice(pIndex, 1);
                 pIndex--;
                 continue;
             }
 
-            // determine direction by checking the previous junction
-            // check with branch was chosen to reach current junction
-            let direction = jIndex === 0 ? Direction.SOUTH : directions[junctions[jIndex-1]?.branches.findIndex(branch => branch && branch?.junctions?.findIndex(j => j?.tile.x === junction.tile.x && j?.tile.y === junction.tile.y) >= 0) || 0];
-
-
-            // Choose a segment (prefer perimeter, then straight lines, then north/west)
-//            next.sort((a, b) => {
-                // preference towards south and east directions (perimeter assuming start is north west)
-//                return a.direction === Direction.SOUTH || a.direction === Direction.EAST ? -1 : 1;
-//            }).sort((a, b) => {
-                // preference towards same direction (straight lines)
-  //              return current.direction === a.direction ? -1 : current.direction === b.direction ? 1 : 0;
-    //        });
-
             // queue next segment or queue a new path
-            next.filter(s => s !== undefined).forEach((segment, index) => {
-                let nextjunction = segment!.junctions.filter(j => j.tile.x !== junction.tile.x || j.tile.y !== junction.tile.y)[0];
+            branches.forEach((branch, index) => {
+                let step = {
+                    direction: branch!.direction,
+                    length: branch!.length,
+                    junction: junctions.filter(j => j.tile.x === branch!.junction!.tile.x && j.tile.y === branch!.junction!.tile.y)[0]
+                };
+
                 if(index === 0) {
-                    path.junctions.push({ ...nextjunction });
-                    path.segments.push({ ...segment! });
+                    path.steps.push(step);
                 } else {
                     paths.push({
-                        junctions: path.junctions.slice(0, -1).concat({ ...nextjunction }),
-                        segments: path.segments.slice(0, -1).concat({ ...segment! })
+                        steps: path.steps.slice(0, -1).concat(step),
                     });
                 }
             });
@@ -214,7 +207,7 @@ function traverse(junctions: Junction[], start: Tile): Path[] {
         }
     }
 
-    return paths.filter(path => path.junctions[path.junctions.length - 1].exit);
+    return paths.filter(path => path.steps[path.steps.length - 1].junction.exit);
 
 }
 
@@ -222,7 +215,9 @@ function partOne(input: string[]): number {
     let tiles: Tile[][] = parseInput(input);
     let junctions: Junction [] = contract(tiles, true);
     return traverse(junctions, junctions[0].tile).reduce((acc, path) => {
-        let pathLength = path.junctions.length - 1 + path.segments.reduce((acc, segment) => acc + segment.length, 0);
+        let pathLength = path.steps.reduce((acc, step) => {
+            return acc + (step?.length || 0);
+        }, path.steps.length - 1);
         return Math.max(acc, pathLength);
     }, 0);
 }
@@ -231,7 +226,9 @@ function partTwo(input: string[]): number {
     let tiles: Tile[][] = parseInput(input);
     let junctions: Junction [] = contract(tiles, false);
     return traverse(junctions, junctions[0].tile).reduce((acc, path) => {
-        let pathLength = path.junctions.length - 1 + path.segments.reduce((acc, segment) => acc + segment.length, 0);
+        let pathLength = path.steps.reduce((acc, step) => {
+            return acc + (step?.length || 0);
+        }, path.steps.length - 1);
         return Math.max(acc, pathLength);
     }, 0);
 }
